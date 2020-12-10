11e3f3050c3e9b16d9580eda4123f8d8a3ce509d
LudoLerma <lermaludovica@gmail.com>
2020-12-10 15:59:33 +0100

    services

--- ejbModule/polimi/db2/gamifyDB/services/Offensive_wordService.java
+++ ejbModule/polimi/db2/gamifyDB/services/Offensive_wordService.java
@@ -1,7 +1,17 @@
 package polimi.db2.gamifyDB.services;
 
+import javax.persistence.PersistenceException;
+import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
+import javax.persistence.NonUniqueResultException;
+import polimi.db2.gamifyDB.entities.Answer;
+import polimi.db2.gamifyDB.entities.Question;
+import polimi.db2.gamifyDB.entities.Review;
+import java.util.List;
+import java.util.Date;
+
 
 @Stateless
 public class Offensive_wordService {
