55b9b083bb317209e7da95154221bb2bc7e04ce7
darklamp <alessandro17.villa@mail.polimi.it>
2020-12-10 16:04:34 +0100

    Updated Offensive_wordService

--- ejbModule/polimi/db2/gamifyDB/services/Offensive_wordService.java
+++ ejbModule/polimi/db2/gamifyDB/services/Offensive_wordService.java
@@ -1,20 +1,28 @@
 package polimi.db2.gamifyDB.services;
 
-import javax.persistence.PersistenceException;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceException;
-import javax.persistence.NonUniqueResultException;
-import polimi.db2.gamifyDB.entities.Answer;
-import polimi.db2.gamifyDB.entities.Question;
-import polimi.db2.gamifyDB.entities.Review;
-import java.util.List;
-import java.util.Date;
 
+import polimi.db2.gamifyDB.entities.Offensive_word;
 
 @Stateless
 public class Offensive_wordService {
 	@PersistenceContext(unitName = "GamifyEJB")
 	private EntityManager em;
+	
+	public Offensive_wordService(){
+	}
+	
+	public List<String> getWords() throws Exception {
+	try {
+		return em.createNamedQuery("Offensive_word.getWords", Offensive_word.class).getResultList().stream().map(arg0 -> arg0.getWord()).collect(Collectors.toList()); 
+	} catch (PersistenceException e) {
+		throw new Exception("Could not retrieve offensive words");
+	}
+}
 }
