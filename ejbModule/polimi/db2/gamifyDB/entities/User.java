dd7f0006b113186a9a46af790ab265a427171b43
LudoLerma <lermaludovica@gmail.com>
2020-12-10 02:08:42 +0100

    services

--- ejbModule/polimi/db2/gamifyDB/entities/User.java
+++ ejbModule/polimi/db2/gamifyDB/entities/User.java
@@ -12,6 +12,7 @@ import java.util.List;
  */
 @Entity
 @NamedQuery(name="User.findAll", query="SELECT u FROM User u")
+@NamedQuery(name = "User.checkCredentials", query = "SELECT r FROM User r  WHERE r.username = ?1 and r.password = ?2")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
---
+++ ejbModule/polimi/db2/gamifyDB/services/Offensive_wordService.java
@@ -0,0 +1,5 @@
+package polimi.db2.gamifyDB.services;
+
+public class Offensive_wordService {
+
+}
