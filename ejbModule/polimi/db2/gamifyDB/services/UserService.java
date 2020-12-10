6a7b4e25b8d69257b0fcb9798941bb1a795d1808
LudoLerma <lermaludovica@gmail.com>
2020-12-10 02:08:42 +0100

    services

---
+++ ejbModule/polimi/db2/gamifyDB/services/UserService.java
@@ -0,0 +1,46 @@
+package polimi.db2.gamifyDB.services;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
+import javax.persistence.NonUniqueResultException;
+import it.polimi.db2.mission.entities.User;
+import it.polimi.db2.mission.exceptions.*;
+import java.util.List;
+
+@Stateless
+public class UserService{
+	@PersistenceContext(unitName = "GamifyEJB")
+	private EntityManager em;
+
+	public UserService(){
+	}
+
+	public User checkCredentials(String usrn, String pwd) throws CredentialsException, NonUniqueResultException {
+		List<User> uList = null;
+		try {
+			uList = em.createNamedQuery("User.checkCredentials", User.class).setParameter(1, usrn).setParameter(2, pwd)
+					.getResultList();
+		} catch (PersistenceException e) {
+			throw new CredentialsException("Could not verify credentals");
+		}
+		if (uList.isEmpty())
+			return null;
+		else if (uList.size() == 1)
+			return uList.get(0);
+		throw new NonUniqueResultException("More than one user registered with same credentials");
+
+	}
+	
+	public User findUserById(){}
+	public User findUserWhoSubmitted(){}
+
+	public void updateProfile(User u) throws UpdateProfileException {
+		try {
+			em.merge(u);
+		} catch (PersistenceException e) {
+			throw new UpdateProfileException("Could not change profile");
+		}
+	}
+}
